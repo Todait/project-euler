589fe1b7cf84c7e13a6723b15fe46dfc64112ba9
Ryan Yoo <yoobh17@gmail.com>
2016-07-04 16:54:36 +0900

    refactor

--- problem-001/ryan/swift/Problem01.swift
+++ problem-001/ryan/swift/Problem01.swift
@@ -1,9 +1,9 @@
+
 import Foundation
 
-func answer(of: Int..., below: Int) -> Int {
+func sum(multiplesOf of: Int..., below: Int) -> Int {
     return (1..<below).filter { i in of.contains { i % $0 == 0 } }.reduce(0, combine: +)
 }
 
-print(answer(3, 5, below: 10) == 23)
-print(answer(3, 5, below: 1000) == 233168)
-
+print(sum(multiplesOf: 3, 5, below: 10) == 23)
+print(sum(multiplesOf: 3, 5, below: 1000) == 233168)
