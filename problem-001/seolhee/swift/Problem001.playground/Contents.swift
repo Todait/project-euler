e7586b67f02d51b2c7c5bf3b4002ce2cb794614c
seolheelee <rain00723@gmail.com>
2017-02-02 15:21:11 +0900

    chore: problem-001

---
+++ problem-001/seolhee/swift/Problem001.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+import XCTest
+
+func getSumOfMultiples(from: Int, to: Int) -> Int? {
+    guard from < to else {
+        return nil
+    }
+    return (from..<to).filter{ $0 % 3 == 0 || $0 % 5 == 0 }.reduce(0, +)
+}
+
+XCTAssertEqual(getSumOfMultiples(from: 1, to: 10), 23)
+XCTAssertEqual(getSumOfMultiples(from: 1, to: 1000), 233168)
