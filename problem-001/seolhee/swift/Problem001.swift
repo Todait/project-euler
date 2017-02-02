57ae68d0b6ed920ac224272af65a1f8a80221ada
seolheelee <rain00723@gmail.com>
2017-02-02 20:03:31 +0900

    filter 컨벤션 수정

--- problem-001/seolhee/swift/Problem001.swift
+++ problem-001/seolhee/swift/Problem001.swift
@@ -5,7 +5,7 @@ func getSumOfMultiples(from: Int, to: Int) -> Int? {
     guard from < to else {
         return nil
     }
-    return (from..<to).filter{ $0 % 3 == 0 || $0 % 5 == 0 }.reduce(0, +)
+    return (from..<to).filter { $0 % 3 == 0 || $0 % 5 == 0 }.reduce(0, +)
 }
 
 XCTAssertEqual(getSumOfMultiples(from: 1, to: 10), 23)
