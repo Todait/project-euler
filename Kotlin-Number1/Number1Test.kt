f847e5f473b7eed7745fdc57c4e1fc82d8cd8302
이재화 <cenacolo34@gmail.com>
2017-02-21 10:18:00 +0900

    refactor : problem1

--- Kotlin-Number1/Number1Test.kt
+++ Kotlin-Number1/Number1Test.kt
@@ -1,63 +1,77 @@
 package lee.eulerproject
 
-import lee.eulerproject.problem.getAnswer
+import lee.eulerproject.problem.getGCD
 import lee.eulerproject.problem.getLCM
+import lee.eulerproject.problem.getNumber1Answer
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
 import kotlin.test.assertEquals
 
 class Number1Test : Spek({
 
-    describe("최소 공배수를 구한다") {
+    describe("1번의 해답") {
 
-        given("두 수가 모두 1이라면") {
-
-            val firstNumber = 1
-            val secondNumber = 1
-
-            on("두 수의 최소 공배수를 구할 때") {
-
-                val value = getLCM(firstNumber, secondNumber)
-                it("1이 나와야 한다") {
-                    assertEquals(value, 1)
-                }
-            }
+        it("1000 보다 작은 자연수 중에서 3 또는 5의 배수의 합은") {
+            assertEquals(getNumber1Answer(5, 3, 1000), 233168)
         }
 
-        given("두 수가 3,5라면") {
-
-            val firstNumber = 3
-            val secondNumber = 5
-
-            on("두 수의 최소 공배수를 구할 때") {
-
-                val value = getLCM(firstNumber, secondNumber)
-                it("15가 나와야 한다") {
-                    assertEquals(value, 15)
-                }
-            }
+        it("10 보다 작은 자연수 중에서 3 또는 5의 배수의 합은") {
+            assertEquals(getNumber1Answer(5, 3, 10), 23)
         }
     }
 
-    describe("1번의 정답을 구한다") {
+    describe("getGCD 가 제대로 값을 구하는지") {
 
-        given("첫번째 수가 3, 두번쨰 수가 5, 제한 숫자가 1000이며") {
+        it("5,3 의 최대공약수는 1이다") {
+            assertEquals(getGCD(5, 3), 1)
+        }
 
-            val firstNumber = 3
-            val secondNumber = 5
-            val limitNumber = 1000
+        it("1,1 의 최대공약수는 1이다") {
+            assertEquals(getGCD(1, 1), 1)
+        }
 
-            on("1000미만의 첫번째 수와 두번째 수의 배수를 더하면") {
+        it("15,3 의 최대공약수는 3이다") {
+            assertEquals(getGCD(15, 3), 3)
+        }
 
-                val value = getAnswer(firstNumber, secondNumber, limitNumber)
-                it("233168이 나와야 한다") {
-                    assertEquals(value, 233168)
-                }
-            }
+        it("10,20 의 최대공약수는 10이다") {
+            assertEquals(getGCD(10, 20), 10)
+        }
+
+        it("20,18 의 최대공약수는 2이다") {
+            assertEquals(getGCD(20, 18), 2)
+        }
+
+        it("-1,5 의 최대공약수는 -1이다") {
+            assertEquals(getGCD(-1, 5), -1)
         }
     }
 
+    describe("getLCM 가 제대로 값을 구하는지") {
+
+        it("5,3 의 최대공배수는 15이다") {
+            assertEquals(getLCM(5, 3), 15)
+        }
+
+        it("15,20 의 최대공배수는 60이다") {
+            assertEquals(getLCM(15, 20), 60)
+        }
+
+        it("1,3 의 최대공배수는 3이다") {
+            assertEquals(getLCM(1, 3), 3)
+        }
+
+        it("4,2 의 최대공배수는 4이다") {
+            assertEquals(getLCM(4, 2), 4)
+        }
+
+        it("10,3 의 최대공배수는 30이다") {
+            assertEquals(getLCM(10, 3), 30)
+        }
+
+        it("4, -10의 최대 공배수는 -1이다") {
+            assertEquals(getLCM(4, -10), -1)
+        }
+    }
 })
