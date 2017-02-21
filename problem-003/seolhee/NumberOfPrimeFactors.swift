47c16814973f04a2d3100cff491ec0c699d0e7d7
seolheelee <rain00723@gmail.com>
2017-02-21 14:07:34 +0900

    오일러 알고리즘 3번 문제 리팩토링및 테스트코드 추가

--- problem-003/seolhee/NumberOfPrimeFactors.swift
+++ problem-003/seolhee/NumberOfPrimeFactors.swift
@@ -8,10 +8,28 @@
 
 import Foundation
 
-func pow(of num: Int) -> Int { return Int(pow(Double(num), 0.5).rounded()) }
-
-func isPrime(number: Int) -> Bool { return number == 2 ? true : (2..<pow(of: number)).first { return number % $0 == 0 } == nil }
-
-func getMaxPrime(of numberOfPrimeFactors: Int) -> Int? {
-    return (2..<numberOfPrimeFactors).reversed().filter { numberOfPrimeFactors % $0 == 0 && isPrime(number: $0) }.first
+extension Int {
+    
+    var isPrime: Bool { return self < 4 ? self > 1 : !(2...self.sqrtInt).contains { self % $0 == 0 } }
+    var sqrtInt: Int { return Int(sqrt(Double(self))) }
+    var primeFactors: [Int] {
+        if self < 4 || self.isPrime {
+            return []
+        }
+        
+        var primeFactors: Set<Int> = []
+        for number in 2...self.sqrtInt {
+            if self % number == 0 {
+                if number.isPrime {
+                    primeFactors.insert(number)
+                }
+                if (self / number).isPrime {
+                    primeFactors.insert(self / number)
+                }
+            }
+        }
+        return primeFactors.sorted()
+    }
+    var maxPrimeFactor: Int? { return self.primeFactors.max() }
+    
 }
--- problem-003/seolhee/NumberOfPrimeFactorsTests.swift
+++ problem-003/seolhee/NumberOfPrimeFactorsTests.swift
@@ -11,11 +11,39 @@ import Nimble
 @testable import EulerProject
 
 class NumberOfPrimeFactorsTests: QuickSpec {
-    
     override func spec() {
-        expect(getMaxPrime(of: 10)).to(equal(5))
-        expect(getMaxPrime(of: 13195)).to(equal(29))
-        expect(getMaxPrime(of: 600851475143)).to(equal(6857))
+        context("fibonacci edge case") {
+            it("") {
+                expect(1.sqrtInt).to(equal(1))
+                expect(2.sqrtInt).to(equal(1))
+                expect(3.sqrtInt).to(equal(1))
+                expect(4.sqrtInt).to(equal(2))
+            }
+            it("4 is not prime number") {
+                expect(1.isPrime).to(equal(false))
+                expect(2.isPrime).to(equal(true))
+                expect(3.isPrime).to(equal(true))
+                expect(4.isPrime).to(equal(false))
+                expect(5.isPrime).to(equal(true))
+                expect(201.isPrime).to(equal(false))
+            }
+            it("prime number havn't prime factors") {
+                expect(1.primeFactors).to(equal([]))
+                expect(2.primeFactors).to(equal([]))
+                expect(5.primeFactors).to(equal([]))
+            }
+            it("non-prime-number express prime factors") {
+                expect(4.primeFactors).to(equal([2]))
+                expect(10.primeFactors).to(equal([2, 5]))
+                expect(30.primeFactors).to(equal([2, 3, 5]))
+                expect(100.primeFactors).to(equal([2, 5]))
+            }
+            it("") {
+                expect(2.maxPrimeFactor).to(beNil())
+                expect(10.maxPrimeFactor).to(equal(5))
+                expect(13195.maxPrimeFactor).to(equal(29))
+                expect(600851475143.maxPrimeFactor).to(equal(6857))
+            }
+        }
     }
-    
 }
