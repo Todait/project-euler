5c31146a0acaacaa835929aeb9197c8f8c55ee92
이재화 <cenacolo34@gmail.com>
2017-02-21 10:21:54 +0900

    refactor : number3

--- Kotlin-Number3/Number3.kt
+++ Kotlin-Number3/Number3.kt
@@ -1,18 +1,22 @@
 package lee.eulerproject.problem
 
 
-fun getNumber3Answer(number: Long): Long? =
-        getDivisiors(number).filter { num ->
-            var judge = true
-            for (n in LongRange(2, Math.sqrt(num.toDouble()).toLong())) {
-                if (num % n == 0.toLong()) {
-                    judge = false
-                    if (!judge) break
-                }
-            }
-            judge
-        }.max()
+fun getNumber3Answer(number: Long): Long = getPrimeFactors(number).max() ?: 0
+
+fun getPrimeFactors(input: Long): MutableSet<Long> {
+    val primeFactors = mutableSetOf<Long>()
+    var number = input
+    var primeFactor = 2L
+    while (number > 1) {
+        if (number % primeFactor == 0L) {
+            primeFactors.add(primeFactor)
+            number /= primeFactor
+        } else
+            primeFactor++
+    }
+    return primeFactors
+}
 
 
-fun getDivisiors(number: Long): List<Long> =
-        LongRange(2, Math.sqrt(number.toDouble()).toLong()).filter { number % it == 0L }
+// feedback
+// 다시 짜기
--- Kotlin-Number3/Number3Test.kt
+++ Kotlin-Number3/Number3Test.kt
@@ -1,12 +1,11 @@
 package lee.eulerproject
 
-import lee.eulerproject.problem.getDivisiors
 import lee.eulerproject.problem.getNumber3Answer
+import lee.eulerproject.problem.getPrimeFactors
+import lee.eulerproject.problem.isPrimeNumber
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
 import kotlin.test.assertEquals
 
 
@@ -14,46 +13,31 @@ class Number3Test : Spek({
 
     describe("3번의 정답을 구한다") {
 
-        given("600851475143이라는 숫자가 있다") {
-
-            val number = 600851475143
-
-            on("해당 수의 소인수 중에서 가장 큰 수는") {
-
-                val value = getNumber3Answer(number)
-                it("6857이다") {
-                    assertEquals(value, 6857)
-                }
-            }
+        it("600851475143에서 제일 큰 소인 수는 6857 이다") {
+            assertEquals(getNumber3Answer(600851475143), 6857)
         }
     }
 
-    describe("getDivisiors 함수기능") {
+    describe("getPrimeFactors 잘 동작을 하는지") {
 
-        given("600851475143이라는 숫자가 있다") {
-
-            val number = 600851475143
-
-            on("600851475143 의 getDivisiors를 구하면") {
-
-                val value = getDivisiors(number)
-                it("모두 0으로 나누어 떨어진다") {
-                    assertEquals(0, value.filter { number % it != 0L }.size)
-                }
-            }
+        it("1까지에서의 소인수는 없다") {
+            assertEquals(getPrimeFactors(1L).isEmpty(), true)
         }
 
-        given("13195이라는 숫자가 있다") {
+        it("2까지에서의 소인수가 잘 뽑아진다") {
+            assertEquals(getPrimeFactors(2L) == mutableSetOf(2L), true)
+        }
 
-            val number = 13195L
+        it("20까지에서의 소인수가 잘 뽑아진다") {
+            assertEquals(getPrimeFactors(20L) == mutableSetOf(2L, 5L), true)
+        }
 
-            on("13195 의 getDivisiors를 구하면") {
+        it("30까지에서의 소인수가 잘 뽑아진다") {
+            assertEquals(getPrimeFactors(30L) == mutableSetOf(2L, 3L, 5L), true)
+        }
 
-                val value = getDivisiors(number)
-                it("모두 0으로 나누어 떨어진다") {
-                    assertEquals(0, value.filter { number % it != 0L }.size)
-                }
-            }
+        it("13195까지에서의 소인수가 잘 뽑아진다") {
+            assertEquals(getPrimeFactors(13195L) == mutableSetOf(5L, 7L, 13L, 29L), true)
         }
     }
 })
