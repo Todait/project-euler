083a881b5939fedbbdf7253265bdf0a71a3f3fba
이재화 <cenacolo34@gmail.com>
2017-03-02 20:15:12 +0900

    jaehwa/solve-problem24 최적화

--- kotlin-problem24/Number24.kt
+++ kotlin-problem24/Number24.kt
@@ -7,26 +7,33 @@ fun getNumber24Answer(): String {
     return getLexicographicOrder(1000000, numberArray)
 }
 
+// 시간 복잡도
+// 배열의 갯수를 n개라 할 때 -> O(n) 번 걸림
 fun getLexicographicOrder(orderNumber: Int, numbers: MutableList<String>): String {
 
     if (orderNumber <= 0)
         return "0번쨰 순서는 없습니다"
 
-    if (orderNumber > getFactorialNumber(numbers.size) ?: 0)
+    var caseNumber: Int = getFactorialNumber(numbers.size) ?: 0
+
+    if (orderNumber > caseNumber)
         return "순서가 초과되었습니다"
+
+    if (caseNumber < 0)
+        return "배열의 사이즈가 음수입니다"
 
     var lexicographicOrder: String = ""
     var dividedNumber: Int = orderNumber - 1
 
     for (i in numbers.size - 1 downTo 1) {
 
-        getFactorialNumber(i)?.let {
-            val index = (dividedNumber / it)
-            dividedNumber = dividedNumber % it
-            lexicographicOrder += numbers[index]
-            numbers.removeAt(index)
-        }
+        caseNumber /= i + 1
+        val index = (dividedNumber / caseNumber)
+        dividedNumber = dividedNumber % caseNumber
+        lexicographicOrder += numbers[index]
+        numbers.removeAt(index)
     }
+
     lexicographicOrder += numbers[0]
     return lexicographicOrder
 }
